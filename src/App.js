95701dd219246c6f1b2d471fd012c94b0a0eb9af
Dvir <dvir1225@gmail.com>
2022-10-15 13:23:09 +0300

    add background images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome'
+import greenBlob from './green-blob.svg'
+import yellowBlob from './yellow-blob.svg'
 
 export default function App() {
   const [gameStarted, setGameStarted] = React.useState(false)
   const [difficulty, setDifficulty] = React.useState('select-difficulty')
   const [gameType, setGameType] = React.useState('select-type')
   const [questions, setQuestions] = React.useState([])
+
+  function getQuestions(url){
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          setQuestions(() => data.results)})
+  }
 
   function startQuiz(e){
     e.preventDefault();
@@ -15,30 +24,19 @@ export default function App() {
       console.log('Please choose quiz settings')
       setGameStarted(false)
     } else if (difficulty === "Any Difficulty" && gameType === "Any Type") {
-      fetch('https://opentdb.com/api.php?amount=10')
-        .then(res => res.json())
-        .then(data => {
-          setQuestions(() => data.results)})
+      getQuestions('https://opentdb.com/api.php?amount=10')
     } else if (difficulty !== "Any Difficulty" && gameType === "Any Type"){
-      fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`)
-        .then(res => res.json())
-        .then(data => {
-          setQuestions(() => data.results)})
+      getQuestions(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`)
     } else if (difficulty === "Any Difficulty" && gameType !== "Any Type"){
-      fetch(`https://opentdb.com/api.php?amount=10&type=${gameType}`)
-        .then(res => res.json())
-        .then(data => {
-          setQuestions(() => data.results)})
+      getQuestions(`https://opentdb.com/api.php?amount=10&type=${gameType}`)
     } else {
-      fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}&type=${gameType}`)
-        .then(res => res.json())
-        .then(data => {
-          setQuestions(() => data.results)})
+      getQuestions(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}&type=${gameType}`)
     }
   }
-
+  
   return (
     <div className="App">
+      <img className="yellowBlob" src={yellowBlob} alt="" />
       {!gameStarted && < Welcome 
       difficulty={difficulty}
       setDifficulty={setDifficulty}
@@ -46,6 +44,7 @@ export default function App() {
       setGameType={setGameType}
       startQuiz={startQuiz}
       />}
+      <img className="greenBlob" src={greenBlob} alt="" />
     </div>
   );
 }
